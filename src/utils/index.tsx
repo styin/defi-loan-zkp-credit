5cbd225d3e93878fbd35b5f5092bea47df950a35
Tianci Yin <sebastian.ytc@outlook.com>
2024-03-06 04:25:04 +0800

    project setup and initial integration of metamask sdk

---
+++ src/utils/index.tsx
@@ -0,0 +1,12 @@
+export const formatBalance = (rawBalance: string) => {
+    // convert rawBalance<string> to a number and divide by 10^18 (1 ether = 10^18 wei)
+    // then round to 2 decimal places
+    const balance = (parseInt(rawBalance) / 1000000000000000000).toFixed(2);
+    return balance;
+};
+
+export const formatChainAsNum = (chainIdHex: string) => {
+    // convert chainIdHex<string> to an integer
+    const chainIdNum = parseInt(chainIdHex);
+    return chainIdNum;
+}
---
+++ src/vite-env.d.ts
@@ -0,0 +1,7 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+/// <reference types="vite/client" />
+
+
+interface Window {
+    ethereum: any;
+}
