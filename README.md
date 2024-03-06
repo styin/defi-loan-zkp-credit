d14bf29b4497394fac41cb23f526887cd0f797b5
Tianci Yin <sebastian.ytc@outlook.com>
2024-03-07 05:29:43 +0800

    completed compartmentalization of frontend and backend

---
+++ README.md
@@ -0,0 +1,13 @@
+# Decentralized Lending Platform with Zero-Knowledge Proof-Based Credit
+
+---
+
+This project is a full-stack implementation of a proposed protocol enabling decentralized lending based on zero-knowledge proof-based credit fostering more efficient virtual asset liquidity.
+
+---
+
+HKUST FYP 2024
+
+- in partial fulfillment of the requirements for COMP 4981 in the Department of Computer Science and Engineering, The Hong Kong University of Science and Technology;
+
+- and in partial fulfillment of the requirements for CPEG 4901 in the Computer Engineering Program Office, The Hong Kong University of Science and Technology.
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,134 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import "./App.css";
+import { useState, useEffect } from "react";
+import { formatBalance, formatChainAsNum } from "../utils";
+import detectEthereumProvider from "@metamask/detect-provider";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    // destructuring assignment from useState hook to get the state and the function to update the state
+    const [hasProvider, setHasProvider] = useState<boolean | null>(null);
+    // initial state for the wallet
+    const initialState = {
+        accounts: [],
+        balance: "",
+        chainId: "",
+    };
+    // destructuring assignment from useState hook to get the state and the function to update the state
+    const [wallet, setWallet] = useState(initialState);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    // status of the connection to MetaMask
+    const [isConnecting, setIsConnecting] = useState(false);
+    const [error, setError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
 
-export default App
+    // this useEffect hook runs after the first render, since it has an empty dependency array
+    useEffect(() => {
+        // refresh the accounts
+        const refreshAccounts = (accounts: any) => {
+            if (accounts.length > 0) {
+                updateWallet(accounts);
+            } else {
+                // if length 0, user is disconnected
+                setWallet(initialState);
+            }
+        };
+
+        // refresh the chain
+        const refreshChain = (chainId: any) => {
+            setWallet((wallet) => ({ ...wallet, chainId }));
+        }
+
+        // async function to get the provider
+        const getProvider = async () => {
+            const provider = await detectEthereumProvider({ silent: true });
+            setHasProvider(Boolean(provider));
+            // if provider exists, get the accounts
+            if (provider) {
+                const accounts = await window.ethereum.request(
+                    { method: "eth_accounts" }
+                );
+                refreshAccounts(accounts);
+                window.ethereum.on("accountsChanged", refreshAccounts);
+                window.ethereum.on("chainChanged", refreshChain);
+            }
+        };
+
+        getProvider();
+        // remove the event listener when the component unmounts
+        return () => {
+            window.ethereum?.removeListener("accountsChanged", refreshAccounts);
+            window.ethereum?.removeListener("chainChanged", refreshChain);
+        }; 
+    });
+
+    // async function to update the wallet
+    const updateWallet = async (accounts: any) => {
+        // update the state with the new accounts and balance
+        const balance = formatBalance(
+            await window.ethereum.request({
+                method: "eth_getBalance",
+                params: [accounts[0], "latest"],
+            })
+        );
+        // update the state with the new chainId
+        const chainId = await window.ethereum.request({
+            method: "eth_chainId",
+        });
+        setWallet({ accounts, balance, chainId });
+    };
+
+    // async function to handle the connection while updating the state
+    const handleConnect = async () => {
+        setIsConnecting(true);
+        await window.ethereum
+            .request({
+                method: "eth_requestAccounts",
+            })
+            .then((accounts: []) => {
+                setError(false);
+                updateWallet(accounts);
+            })
+            .catch((err: any) => {
+                setError(true);
+                setErrorMessage(err.message);
+            });
+        setIsConnecting(false);
+    };
+
+    const disableConnect = Boolean(wallet) && isConnecting;
+
+    // JSX
+    return (
+        <div className="App">
+            {/* conditional rendering */}
+            <div>
+                Injected Provider {hasProvider ? "DOES" : "DOES NOT"} Exist
+            </div>
+
+            {window.ethereum?.isMetaMask && wallet.accounts.length < 1 && (                       
+                <button disabled={disableConnect} onClick={handleConnect}>
+                    Connect MetaMask
+                </button>
+            )}
+
+            {wallet.accounts.length > 0 && (
+                <>
+                    <div>Wallet Accounts: {wallet.accounts[0]}</div>
+                    <div>Wallet Balance: {wallet.balance}</div>
+                    <div>Hex ChainId: {wallet.chainId}</div>
+                    <div>
+                        Numeric ChainId: {formatChainAsNum(wallet.chainId)}
+                    </div>
+                </>
+            )}
+            {error && (
+                <div onClick={() => setError(false)}>
+                    <strong>Error:</strong> {errorMessage}
+                </div>
+            )}
+        </div>
+    );
+};
+
+// this allows the App component to be imported in other files
+export default App;
