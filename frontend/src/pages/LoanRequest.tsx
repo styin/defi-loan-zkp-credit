ad377a2de743c4ca621565c182379e7a046268e5
Tianci Yin <sebastian.ytc@outlook.com>
2024-03-24 01:13:25 +0800

    fixed routing for the loan-request pages with pseudo-nested urls

--- frontend/src/pages/LoanRequest.tsx
+++ frontend/src/pages/LoanRequest.tsx
@@ -6,10 +6,10 @@ const LoanRequest: React.FC = () => {
         <div>
             <h1>Loan Request Page</h1>
             {/*loan request's homepage*/}
-            <Link to="/post-request">
+            <Link to="post-request">
                 <button>Post request</button>
             </Link>
-            <Link to="/request-list">
+            <Link to="request-list">
                 <button>Check request list</button>
             </Link>
         </div>
--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -8,19 +8,32 @@ import AuthGuard from './AuthGuard';
 import RequestList from '../pages/RequestList';
 import Login from '../pages/Login';
 import MainPage from '../pages/MainPage';
+import RequestForm from '../pages/RequestForm';
+import LoanRequest from '../pages/LoanRequest';
 
 const RouterConfig: React.FC = () => {
     return (
         <BrowserRouter>
             <Routes>
-                /* Route 1: App */
+                {/* Route 1: App */}
                 <Route path="/" element={<MainPage />} />
-                /* Route 2: Dashboard */
+
+                {/* Route 2: Dashboard */}
                 <Route path="/dashboard" element={
-                    <AuthGuard> 
-                        <RequestList /> 
-                    </AuthGuard>} />
-                /* Route 3: Login */
+                    <AuthGuard> <LoanRequest /> </AuthGuard>
+                }/>
+
+                {/* Pseudo-Subroute 3: Post Request */}
+                <Route path="/dashboard/post-request" element={
+                    <AuthGuard> <RequestForm /> </AuthGuard>
+                }/>
+
+                {/* Pseudo-Subroute 4: Request List */}
+                <Route path="/dashboard/request-list" element={
+                    <AuthGuard> <RequestList /> </AuthGuard>
+                }/>
+
+                {/* Route 3: Login */}
                 <Route path="/login" element={<Login />} />
             </Routes>
         </BrowserRouter>
