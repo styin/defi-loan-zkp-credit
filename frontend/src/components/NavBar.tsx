c965e6db5488d026f5f11a36ed265b719df8cb1d
Tin04 <oscargu5763@gmail.com>
2024-03-29 00:29:23 +0800

    style: changed the appearance of NavBar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import '../styles/NavBar.css';
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
+    <nav className="navbar">
+      <ul className="nav-list">
         <li>
           <Link to="/">Home</Link>
         </li>
