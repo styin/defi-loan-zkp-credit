d15370b18c4077cf9f250955cbc920b0444bcbc6
Tin04 <oscargu5763@gmail.com>
2024-04-14 16:29:01 +0800

    allow backend server to read frontend requests

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bodyParser from 'body-parser';
 import mongoose, { mongo } from "mongoose";
 
 import { config } from "dotenv";
@@ -8,10 +9,21 @@ const PORT = process.env.PORT || 5000;
 
 // model imports
 import LoanRequest from "./models/loan_request";
-
+import EncryptedMessage from "./models/encrypted_message";
 
 
 const app = express();
+// Middleware to parse JSON data
+app.use(bodyParser.json());
+
+// Enable CORS
+const host = 'http://localhost:5173';
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', host);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 
 // TODO [Seb] This is a dummy route for the loan request. It is not complete and will be updated later.
 app.post("/api/loan_req", async (req, res) => {
@@ -37,6 +49,25 @@ app.post("/api/loan_req", async (req, res) => {
         .catch((err) => {
             console.log("[ERROR] Error saving loan request: ", err);
         });
+});
+
+app.post('/api/send_message', async (req, res) => {
+    const { senderPK, receiverPK, encryptedMessage, signature } = req.body;
+  
+    const newMessage = new EncryptedMessage({
+      senderPK,
+      receiverPK,
+      encryptedMessage,
+      signature
+    })
+  
+    await newMessage.save()
+      .then(() => {
+        res.status(201).json({ message: 'Message saved successfully' });
+      })
+      .catch((error) => {
+        res.status(500).json({ error: 'Failed to save message' });
+      });
 });
 
 // database connection
