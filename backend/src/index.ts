3772cd18b3f96743f9f42251e0af606796756ac2
Tianci Yin <sebastian.ytc@outlook.com>
2024-04-17 23:40:26 +0800

    backend docs

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from 'body-parser';
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
 
 import { config } from "dotenv";
 config();
@@ -11,7 +11,6 @@ const PORT = process.env.PORT || 3000;
 import LoanRequest from "./models/loan_request";
 import EncryptedMessage from "./models/encrypted_message";
 
-
 const app = express();
 // Middleware to parse JSON data
 app.use(bodyParser.json());
@@ -19,70 +18,83 @@ app.use(bodyParser.json());
 // Enable CORS
 const host = 'http://localhost:5173';
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', host);
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+  res.setHeader('Access-Control-Allow-Origin', host);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
 });
 
-// Endpoint for creating a new loan request
+/**
+ * Endpoint for creating a new loan request.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 app.post("/api/post_request", async (req, res) => {
-    const { walletAddress, amount, discountedAmount, duration, additionalNotes } = req.body;
-    // create a new loan request
-    const loanRequest = new LoanRequest({
-            walletAddress,
-            amount,
-            discountedAmount,
-            duration,
-            additionalNotes
+  const { walletAddress, amount, discountedAmount, duration, additionalNotes } = req.body;
+  // create a new loan request
+  const loanRequest = new LoanRequest({
+      walletAddress,
+      amount,
+      discountedAmount,
+      duration,
+      additionalNotes
+  });
+  // save the loan request to the database
+  await loanRequest.save()
+    .then(() => {
+      res.status(201).json({ message: 'Loan request saved successfully' });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Failed to save loan request' });
     });
-    // save the loan request to the database
-    await loanRequest.save()
-        .then(() => {
-            res.status(201).json({ message: 'Loan request saved successfully' });
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed to save loan request' });
-        });
 });
 
-// Endpoint for getting all loan requests
+/**
+ * Endpoint for getting all loan requests.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 app.get("/api/get_requests", async (req, res) => {
-    // get all loan requests from the database
-    await LoanRequest.find()
-        .then((requests) => {
-            res.status(200).json({ requests });
-        })
-        .catch((error) => {
-            res.status(500).json({ error: 'Failed to get loan requests' });
-        });
+  // get all loan requests from the database
+  await LoanRequest.find()
+    .then((requests) => {
+      res.status(200).json({ requests });
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'Failed to get loan requests' });
+    });
 });
 
+/**
+ * Endpoint for sending a message.
+ * @param req - The request object.
+ * @param res - The response object.
+ */
 app.post('/api/send_message', async (req, res) => {
-    const { senderPK, receiverPK, encryptionPackage } = req.body;
-    // create a new message
-    const newMessage = new EncryptedMessage({
-      senderPK,
-      receiverPK,
-      encryptionPackage
+  const { senderPK, receiverPK, encryptionPackage } = req.body;
+  // create a new message
+  const newMessage = new EncryptedMessage({
+    senderPK,
+    receiverPK,
+    encryptionPackage
+  });
+  // save the message to the database
+  await newMessage.save()
+    .then(() => {
+    res.status(201).json({ message: 'Message saved successfully' });
+    })
+    .catch((error) => {
+    res.status(500).json({ error: 'Failed to save message' });
     });
-    // save the message to the database
-    await newMessage.save()
-      .then(() => {
-        res.status(201).json({ message: 'Message saved successfully' });
-      })
-      .catch((error) => {
-        res.status(500).json({ error: 'Failed to save message' });
-      });
 });
 
 // database connection
 const db = mongoose.connect(mongoURI)
-    .then(() => {
-        console.log("[STARTUP] Connected to the database!");
-        app.listen(PORT);
-        console.log(`[STARTUP] Server is running on port ${PORT}!`);
-    })
-    .catch((err) => {
-        console.log("Error connecting to the database: ", err);
-    });
+  .then(() => {
+    console.log("[STARTUP] Connected to the database!");
+    app.listen(PORT);
+    console.log(`[STARTUP] Server is running on port ${PORT}!`);
+  })
+  .catch((err) => {
+    console.log("Error connecting to the database: ", err);
+  });
