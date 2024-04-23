5e6629aaf01a9ce4d415d5c5445316230a6e40e5
Tianci Yin <sebastian.ytc@outlook.com>
2024-04-23 23:45:29 +0800

    remastered UI elements

--- frontend/src/components/RequestForm.tsx
+++ frontend/src/components/RequestForm.tsx
@@ -32,13 +32,12 @@ const RequestForm: React.FC = () => {
       formData.rsaPK,
       walletAddress,
       ""
-    )
+    );
     setFormData({
       ...formData,
       signedRSAPK: signature,
     });
-  }
-
+  };
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -65,33 +64,33 @@ const RequestForm: React.FC = () => {
 
     const walletAddress = wallet.accounts[0]; // Assuming the first account is the active one
     fetch(backendURL + "/api/post_request", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({walletAddress, ...formData}),
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to submit form');
-            }
-            return response.json();
-        })
-        .then(data => {
-            console.log('Form submitted successfully! Response:', data);
-            window.alert('Form submitted successfully!');
-            setFormData({
-              rsaPK: "",
-              signedRSAPK: "",
-              amount: "",
-              discountedAmount: "",
-              duration: "",
-              additionalNotes: "",
-            });
-        })
-        .catch(error => {
-            console.error('Error submitting form:', error);
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ walletAddress, ...formData }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to submit form");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("Form submitted successfully! Response:", data);
+        window.alert("Form submitted successfully!");
+        setFormData({
+          rsaPK: "",
+          signedRSAPK: "",
+          amount: "",
+          discountedAmount: "",
+          duration: "",
+          additionalNotes: "",
         });
+      })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+      });
   };
 
   return (
@@ -105,7 +104,7 @@ const RequestForm: React.FC = () => {
         className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-xl xl:p-0"
       >
         <div className="flex flex-col gap-6 md:p-4">
-        <label className="formlabel">
+          <label className="formlabel">
             <span className="block text-sm font-semibold leading-6 text-gray-600">
               <a className="text-red-600"> * </a> RSA public key
             </span>
@@ -119,20 +118,29 @@ const RequestForm: React.FC = () => {
               required={true}
             />
           </label>
-          <label className="formlabel">
+          <label className="formlabel flex-1">
             <span className="block text-sm font-semibold leading-6 text-gray-600">
               Signed RSA public key
             </span>
-            <input
-              className="formInput flex-2 block w-full p-2 text-gray-500 italic border border-gray-300 rounded-lg bg-gray-50 text-xs"
-              type="text"
-              name="signedRSAPK"
-              value={formData.signedRSAPK}
-              onChange={handleChange}
-              placeholder="Sign on your public key above by pressing on the `Sign` button"
-              required={true}
-              readOnly={true}
-            />
+            <div className="flex flex-row max-h-10 gap-4">
+              <input
+                className="formInput block w-full p-2 text-gray-500 italic border border-gray-300 rounded-lg bg-gray-50 text-xs"
+                type="text"
+                name="signedRSAPK"
+                value={formData.signedRSAPK}
+                onChange={handleChange}
+                placeholder="Sign on your public key above by pressing on the `Sign` button"
+                required={true}
+                readOnly={true}
+              />
+              <button
+                className=" text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+                type="button"
+                onClick={signRsaPK}
+              >
+                Sign
+              </button>
+            </div>
           </label>
           <label className="formlabel">
             <span className="block text-sm font-semibold leading-6 text-gray-600">
@@ -150,7 +158,7 @@ const RequestForm: React.FC = () => {
           </label>
           <label className="formlabel">
             <span className="block text-sm font-semibold leading-6 text-gray-600">
-            <a className="text-red-600"> * </a> Minimum discounted amount
+              <a className="text-red-600"> * </a> Minimum discounted amount
             </span>
             <input
               className="formInput flex-2 block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs"
@@ -164,7 +172,7 @@ const RequestForm: React.FC = () => {
           </label>
           <label className="formlabel">
             <span className="block text-sm font-semibold leading-6 text-gray-600">
-            <a className="text-red-600"> * </a> Loan duration (in days)
+              <a className="text-red-600"> * </a> Loan duration (in days)
             </span>
             <input
               className="formInput flex-2 block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs"
@@ -181,7 +189,7 @@ const RequestForm: React.FC = () => {
               Additional notes
             </span>
             <textarea
-              className="block p-2.5 w-full text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+              className="h-32 block p-2.5 w-full text-xs text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
               name="additionalNotes"
               value={formData.additionalNotes}
               onChange={handleChange}
@@ -193,13 +201,6 @@ const RequestForm: React.FC = () => {
             type="submit"
           >
             Submit
-          </button>
-          <button
-            className="text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
-            type="button"
-            onClick={signRsaPK}
-          >
-            Sign
           </button>
         </div>
       </form>
