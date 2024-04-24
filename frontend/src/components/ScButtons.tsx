57e0a27e6a4c151954a2b3b4d27a09f01c4bac78
Tianci Yin <sebastian.ytc@outlook.com>
2024-04-24 18:32:51 +0800

    Added copy to clipboard functionality for better UX; added etherscan href

--- frontend/src/components/ScButtons.tsx
+++ frontend/src/components/ScButtons.tsx
@@ -189,6 +189,16 @@ const ScButtons: React.FC = () => {
     });
   };
 
+  const handleCopyToClipboard = (text : string) => {
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        console.log("Text copied to clipboard:", text);
+      })
+      .catch((error) => {
+        console.error("Error copying text to clipboard:", error);
+      });
+  };
+
   const handleSubmitCreateCmt = (e: React.FormEvent) => {
     e.preventDefault();
     setTransactionHash("");
@@ -263,9 +273,34 @@ const ScButtons: React.FC = () => {
               <span className="block text-sm font-semibold leading-6 text-gray-600">
                 Tx Hash
               </span>
-              <span className="text-sm font-semibold leading-6 text-gray-900">
-                {transactionHash}
-              </span>
+              <div className="flex flex-row">
+                <a
+                  href={`https://sepolia.etherscan.io/tx/${transactionHash}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-sm font-semibold leading-6 text-gray-900 hover:text-blue-500"
+                >
+                  {transactionHash}
+                </a>
+                <button
+                  data-copy-to-clipboard-target="npm-install-copy-button"
+                  data-tooltip-target="tooltip-copy-npm-install-copy-button"
+                  className="flex-1 md:ml-2"
+                  onClick={() => handleCopyToClipboard(transactionHash)}
+                >
+                  <span id="default-icon">
+                    <svg
+                      className="w-3.5 h-3.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 18 20"
+                    >
+                      <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                    </svg>
+                  </span>
+                </button>
+              </div>
             </label>
           )}
 
@@ -275,9 +310,29 @@ const ScButtons: React.FC = () => {
               <span className="block text-sm font-semibold leading-6 text-gray-600">
                 Commitment Identifier
               </span>
-              <span className="text-sm font-semibold leading-6 text-gray-900">
-                {commitmentIdentifier}
-              </span>
+              <div className="flex flex-row">
+                <span className="text-sm font-semibold leading-6 text-gray-900">
+                  {commitmentIdentifier}
+                </span>
+                <button
+                  data-copy-to-clipboard-target="npm-install-copy-button"
+                  data-tooltip-target="tooltip-copy-npm-install-copy-button"
+                  className="flex-1 md:ml-2"
+                  onClick={() => handleCopyToClipboard(commitmentIdentifier)}
+                >
+                  <span id="default-icon">
+                    <svg
+                      className="w-3.5 h-3.5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="currentColor"
+                      viewBox="0 0 18 20"
+                    >
+                      <path d="M16 1h-3.278A1.992 1.992 0 0 0 11 0H7a1.993 1.993 0 0 0-1.722 1H2a2 2 0 0 0-2 2v15a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V3a2 2 0 0 0-2-2Zm-3 14H5a1 1 0 0 1 0-2h8a1 1 0 0 1 0 2Zm0-4H5a1 1 0 0 1 0-2h8a1 1 0 1 1 0 2Zm0-5H5a1 1 0 0 1 0-2h2V2h4v2h2a1 1 0 1 1 0 2Z" />
+                    </svg>
+                  </span>
+                </button>
+              </div>
             </label>
           )}
 
